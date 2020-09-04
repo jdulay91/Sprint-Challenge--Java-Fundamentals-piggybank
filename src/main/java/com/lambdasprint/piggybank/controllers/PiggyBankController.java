1d5f493424ee839c5fb964ebb122eab8f92d64c5
Julliann Dulay <jullianndulay@gmail.com>
2020-09-04 09:28:43 -0700

    MVP DONE

--- src/main/java/com/lambdasprint/piggybank/controllers/PiggyBankController.java
+++ src/main/java/com/lambdasprint/piggybank/controllers/PiggyBankController.java
@@ -28,6 +28,21 @@ public class PiggyBankController {
         }
         return tempList;
     }
+    private void getTotalCount(List<PiggyBank> coinList,String name){
+        int totalCount = 0;
+        for(PiggyBank p : coinList){
+            totalCount += p.getQuantity();
+        }
+        if(totalCount == 1) {
+            System.out.println(totalCount + name);
+        } else {
+            if(name.equals("Penny")){
+                System.out.println(totalCount + "Pennies");
+            }else{
+                System.out.println(totalCount + name + "s");
+            }
+        }
+    }
     @GetMapping( value = "/total", produces = {"application/json"})
     public ResponseEntity<?> listTotalCoins(){
 
@@ -39,11 +54,16 @@ public class PiggyBankController {
         List<PiggyBank> dimeList = findPiggies(myList, e-> e.getName().equals("Dime"));
         List<PiggyBank> pennyList = findPiggies(myList, e-> e.getName().equals("Penny"));
         List<PiggyBank> dollarList = findPiggies(myList, e-> e.getName().equals("Dollar"));
-
         double total = 0.0;
         for(PiggyBank p : myList){
             total+= p.getQuantity() * p.getValue();
         }
+
+        getTotalCount(quarterList,"Quarter");
+        getTotalCount(nickelList,"Nickel");
+        getTotalCount(dimeList,"Dime");
+        getTotalCount(pennyList,"Penny");
+        getTotalCount(dollarList,"Dollar");
 
         System.out.println("The piggy Bank holds " + total);
         return new ResponseEntity<>(dollarList, HttpStatus.OK);
